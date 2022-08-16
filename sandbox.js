d01b37fd03dd55344a694f6d35ee190e2183de0d
loyeh <104890317+loyeh@users.noreply.github.com>
2022-08-17 02:33:54 +0430

    completed stopwatch code

--- sandbox.js
+++ sandbox.js
@@ -18,12 +18,13 @@ const clock = () => {
 	let second = insertZerro(date1.getSeconds());
 	dd.innerHTML = day + " " + month + "/" + date;
 	pp.innerHTML = hour + ":" + minute + "</br>" + second;
-	larg.innerHTML = hour + ":" + minute + ":" + second;
+	// larg.innerHTML = hour + ":" + minute + ":" + second;
 };
 const largeClock = () => {
 	let page1 = document.getElementById("page1");
 	page1.innerHTML = '<div id="larg"></div> <a href="index.html">WATCH</a>';
 	const larg = document.getElementById("larg");
+	larg.innerHTML = hour + ":" + minute + ":" + second;
 	clock();
 	larg.style.display = "block";
 	larg.style.backgroundColor = "black";
@@ -31,22 +32,38 @@ const largeClock = () => {
 
 const mode = () => {
 	mm.innerHTML =
-		'<ul class="menue"> <li><input type="button" value="Timer" onclick="timermode()"></li> <li><input type="button" value="Stop Watch" onclick="stopwatch()"></li>  <li><input type="button" value="Location" onclick="location()"></li> <li><input type="button" value="Back" onclick="location.reload()"></li> </ul >';
+		'<ul class="menue"> <li><input type="button" value="timer" onclick="timermode()"></li> <li><input type="button" value="Stop Watch" onclick="stopWatchMode()"></li>  <li><input type="button" value="Location" onclick="location()"></li> <li><input type="button" value="Back" onclick="location.reload()"></li> </ul >';
 };
 let s = 0;
 let m = 0;
 let ms = 0;
 var x;
-
-const timermode = () => {
+function timerMode() {
 	mm.innerHTML =
-		'<p class="center" id="timer">00:00</br><span>00</span></p><input type="button" class="date center" id="start" value="start">';
+		'<p class="center" id="stopWatch">00:00</br><span>00</span></p><input type="button" class="date start center" id="start" value="START"><input type="button" class="date reset center" id="reset" value="RESET">';
 	const ts = document.getElementById("start");
+	const tr = document.getElementById("reset");
 	ts.onclick = function () {
 		start();
 	};
-	const tt = document.getElementById("timer");
-	function timer() {
+	tr.onclick = function () {
+		reset();
+	};
+}
+
+const stopWatchMode = () => {
+	mm.innerHTML =
+		'<p class="center" id="stopWatch">00:00</br><span>00</span></p><input type="button" class="date start center" id="start" value="START"><input type="button" class="date reset center" id="reset" value="RESET">';
+	const ts = document.getElementById("start");
+	const tr = document.getElementById("reset");
+	ts.onclick = function () {
+		start();
+	};
+	tr.onclick = function () {
+		reset();
+	};
+	const tt = document.getElementById("stopWatch");
+	function stopWatch() {
 		if (ms > 99) {
 			s++;
 			ms = 0;
@@ -64,18 +81,30 @@ const timermode = () => {
 	}
 	function start() {
 		clearInterval(x);
-		x = setInterval(timer, 10);
-		ts.value = "stop";
+		x = setInterval(stopWatch, 10);
+		ts.value = "STOP";
+		// ts.style = "transform: translate(150%, -200%) rotate(60deg);";
 		ts.onclick = function () {
 			stop();
 		};
 	}
 	function stop() {
 		clearInterval(x);
-		ts.value = "start";
+		ts.value = "START";
 		ts.onclick = function () {
 			start();
 		};
+	}
+	function reset() {
+		clearInterval(x);
+		ms = 0;
+		s = 0;
+		m = 0;
+		ts.value = "START";
+		ts.onclick = function () {
+			start();
+		};
+		tt.innerHTML = "00:00</br><span>00</span>";
 	}
 };
 let time = setInterval(clock, 500);
